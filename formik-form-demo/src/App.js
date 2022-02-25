a54ad45e228ce645681a69da5f7f344897f4d601
bhavyakaria <bhavyakaria@yahoo.in>
2022-02-25 22:53:41 +0530

    chore: added dynamic components

--- formik-form-demo/src/App.js
+++ formik-form-demo/src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
-import { useFormik } from 'formik';
+import { FieldArray, Form, Formik, useFormik } from 'formik';
+import * as yup from "yup";
 
 function App() {
+
+  const validdationSchema = yup.object().shape({
+    email: yup.string(),
+    mobileNumbers: yup.array(yup.string())
+  });
 
   const formik = useFormik({
     initialValues: {
@@ -13,13 +19,29 @@ function App() {
     }
   });
 
+  const updateMobileNumbers = (e) => {
+    formik.values.mobileNumbers.push("");
+  }
+
   return (
     <div className="App">
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="email">Email Address</label>
-        <input id="email" name="email" type="email" onChange={formik.handleChange} value={formik.values.email}></input>
-        <button type="submit">Submit</button>
-      </form>
+      <Formik initialValues={formik.initialValues} validationSchema={validdationSchema} onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input id="email" name="email" type="email" onChange={formik.handleChange} value={formik.values.email}></input>
+          <button onClick={updateMobileNumbers}>Update Mobile Numbers</button>
+          <div style={{ display: 'flex' }}>
+            <FieldArray name="mobileNumbers">
+              {() => (formik.values.mobileNumbers.map((number, index) => {
+                return (
+                  <><input type={"text"} /></>
+                )
+              }))}
+            </FieldArray>
+          </div>
+          <button type="submit">Submit</button>
+        </div>
+      </Formik>
     </div>
   );
 }
--- formik-form-demo/src/index.js
+++ formik-form-demo/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FriendList } from './FriendList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FriendList />
   </React.StrictMode>,
   document.getElementById('root')
 );
